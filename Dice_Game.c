450aefead88378f6de6e62de37860eb0e0c2eea2
s1260010 <s1260010@ilab2dc12.u-aizu.ac.jp>
2019-07-11 18:27:01 +0900

    change win or lose part

--- Dice_Game.c
+++ Dice_Game.c
@@ -21,8 +21,8 @@ int main(){
   }
   printf("Total value: %d\n", sum);
 
-  if(sum > 7) printf("\nYou won!\n");
-  else printf("\nYou lost...\n");
+  if(sum > 7) printf("\n%s won!\n", name);
+  else printf("\n%s lost...\n", name);
 
   return 0;
 }
